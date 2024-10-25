75bcabfa926256f2af56eed70b997d636895134f
Jamuelton <jamueltonangelim@gmail.com>
2024-10-24 21:57:14 -0300

    pag 3 ready

--- src/pages/page3.lua
+++ src/pages/page3.lua
@@ -4,12 +4,49 @@ local scene = composer.newScene()
 
 local button = require("src.components.button")
 
+local currentIndex = 1
+
+local images = {}
+
+local function changeImage(sceneGroup, index)
+   
+    for i = 1, #images do
+        images[i].isVisible = false
+    end
+    
+    images[index].isVisible = true
+end
+
+local function onShake(e)
+    
+    if math.abs(e.xInstant) > 1 or math.abs(e.yInstant) > 1 then
+        
+        if currentIndex < #images then
+            currentIndex = currentIndex + 1
+        else
+            currentIndex = 1
+        end
+        changeImage(scene.view, currentIndex)
+    end
+end
+
 function scene:create(event)
     local sceneGroup = self.view
     
     local capa = display.newImageRect(sceneGroup, "src/assets/pages/pag3.png", 768, 1024)
     capa.x = display.contentCenterX
     capa.y = display.contentCenterY
+
+    images[1] = display.newImageRect(sceneGroup, "src/assets/page3/multidao.png", 425, 425)
+    images[2] = display.newImageRect(sceneGroup, "src/assets/page3/separados.png", 425, 425)
+
+    for i = 1, #images do
+        images[i].x = display.contentCenterX
+        images[i].y = display.contentCenterY + 180
+        images[i].isVisible = false
+    end
+
+    changeImage(sceneGroup, currentIndex)
     
     local nextBtn = button.new(
         display.contentCenterX + 300,
@@ -40,9 +77,16 @@ function scene:create(event)
         end
     )
     sceneGroup:insert(soundBtn)
+
+    Runtime:addEventListener("accelerometer", onShake)
    
 end
 
+function scene:destroy(event)
+    Runtime:removeEventListener("accelerometer", onShake)
+end
+
 scene:addEventListener("create", scene)
+scene:addEventListener("destroy", scene)
 
 return scene

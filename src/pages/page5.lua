06bdb6cd1c91bbdacef8fa6af7c91b4747e17619
Jamuelton <jamueltonangelim@gmail.com>
2024-10-25 23:31:26 -0300

    pag 5 completa

--- src/pages/page5.lua
+++ src/pages/page5.lua
@@ -4,12 +4,51 @@ local scene = composer.newScene()
 
 local button = require("src.components.button")
 
+local labImage, scientistImage, parkImage, centralImage
+
+local function changeImage(image)
+
+    labImage.isVisible = false
+    scientistImage.isVisible = false
+    parkImage.isVisible = false
+
+    if centralImage then
+        centralImage:removeSelf()
+        centralImage = nil
+    end
+
+    centralImage = display.newImageRect(scene.view, image, 681, 265)
+    centralImage.x, centralImage.y = display.contentCenterX, display.contentCenterY + 310
+end
+
+local function onTouch(event)
+    if event.phase == "ended" then
+        if event.target == labImage then
+            changeImage("src/assets/page5/cientistaLab.png") 
+        elseif event.target == parkImage then
+            changeImage("src/assets/page5/cientistaParque.png") 
+        end
+    end
+    return true
+end
+
 function scene:create(event)
     local sceneGroup = self.view
     
     local capa = display.newImageRect(sceneGroup, "src/assets/pages/pag5.png", 768, 1024)
     capa.x = display.contentCenterX
     capa.y = display.contentCenterY
+
+    labImage = display.newImageRect(sceneGroup, "src/assets/page5/laboratorio.png", 200, 162)
+    labImage.x, labImage.y = display.contentCenterX - 250, display.contentCenterY + 300
+    labImage:addEventListener("touch", onTouch)
+
+    scientistImage = display.newImageRect(sceneGroup, "src/assets/page5/cientistas.png", 125, 249)
+    scientistImage.x, scientistImage.y = display.contentCenterX, display.contentCenterY + 300
+
+    parkImage = display.newImageRect(sceneGroup, "src/assets/page5/parque.png", 200, 162)
+    parkImage.x, parkImage.y = display.contentCenterX + 250, display.contentCenterY + 300
+    parkImage:addEventListener("touch", onTouch)
     
     local nextBtn = button.new(
         display.contentCenterX + 300,
